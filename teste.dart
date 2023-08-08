d3948ab0d8e027f44335520add397a18efb36c4f
unknown <Bi3ldogera>
2023-08-07 21:02:19 -0300

    Métodos getter e setters implementados

---
+++ teste.dart
@@ -0,0 +1,10 @@
+class Teste {
+  var nome, nota;
+
+  String getNome() => this.nome;
+  int getNota() => this.nota;
+
+  void setNome(String nom) {this.nome = nom;}
+  void setNota(int not) {this.nota = not;} 
+
+}
